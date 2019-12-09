16e35ec3dd903f71bdf2b4716ae891364611c745
Alexander Böhm <tools@boehm.work>
2019-12-09 12:26:59 +0100

    Reduce the service data from six to four topics

--- frontend/src/components/main/services/services.data.ts
+++ frontend/src/components/main/services/services.data.ts
@@ -1,27 +1,14 @@
-import { faCodepen } from "@fortawesome/free-brands-svg-icons";
 import { faLifeRing } from "@fortawesome/free-regular-svg-icons";
-import { faChartLine, faPaintBrush, faDesktop, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { faChartLine, faPaintBrush, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 
 import { ServiceModel } from "./service.model";
 
 export const serviceData: ServiceModel[] = [
   {
-    icon: faCodepen,
-    header: "Branding",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nostrum ipsa saepe sapiente reprehenderit vero aperiam animi eum blanditiis id."
-  },
-  {
     icon: faPaintBrush,
     header: "Web Design",
     text:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum adipisci minus mollitia similique doloremque in corporis provident facere, aut laboriosam?"
-  },
-  {
-    icon: faChartLine,
-    header: "Search Engine Optimization",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veniam iure quos consectetur ea itaque asperiores quibusdam odit consequuntur. Praesentium."
   },
   {
     icon: faLaptopCode,
@@ -30,14 +17,14 @@ export const serviceData: ServiceModel[] = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident veritatis aspernatur vitae est sunt ut harum atque quidem incidunt doloribus."
   },
   {
-    icon: faDesktop,
-    header: "User Interface",
+    icon: faChartLine,
+    header: "Search Engine Optimization",
     text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis natus nobis aut doloremque unde perferendis assumenda labore debitis ex excepturi?"
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam veniam iure quos consectetur ea itaque asperiores quibusdam odit consequuntur. Praesentium."
   },
   {
     icon: faLifeRing,
-    header: "Hepl &amp; Support",
+    header: "Help & Support",
     text:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor similique rerum tenetur deserunt hic ad nemo eveniet totam! Quo, omnis."
   }
