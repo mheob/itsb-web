890a7c5d6e57d2f663fc86e98ab942a4217b21a9
Alexander Böhm <tools@boehm.work>
2019-12-09 12:15:31 +0100

    Add a ScrolltopButton

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import Navigation from "./mobileNavigation/Navigation";
@@ -7,10 +7,27 @@ import Home from "./main/Home";
 import Impress from "./main/Impress";
 import Privacy from "./main/Privacy";
 import ScrollToTop from "./shared/ScrollToTop";
+import ScrollUpButton from "./shared/ScrollTopButton";
+import { Waypoint } from "react-waypoint";
+
+type ScrollUpButtonState = {
+  isVisibleButton: boolean;
+};
 
 const App: React.FC = () => {
+  const [visibility, setVisibility] = useState<ScrollUpButtonState>({ isVisibleButton: false });
+
+  const scrollUpButtonHandler = (isEntered: boolean) => {
+    setVisibility({ isVisibleButton: isEntered });
+  };
+
   return (
     <Router>
+      <Waypoint
+        topOffset={-600}
+        onEnter={scrollUpButtonHandler.bind(null, false)}
+        onLeave={scrollUpButtonHandler.bind(null, true)}
+      />
       <Navigation />
       <Aside />
       <ScrollToTop>
@@ -27,6 +44,7 @@ const App: React.FC = () => {
           <Redirect to="/" />
         </Switch>
       </ScrollToTop>
+      <ScrollUpButton isVisible={visibility.isVisibleButton} />
     </Router>
   );
 };
