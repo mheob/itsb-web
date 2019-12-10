186b826a68147ab4a75e4eb32db66095a246c332
Alexander Böhm <tools@boehm.work>
2019-12-10 15:00:34 +0100

    Fix closing the offcanvas on hit a link

--- frontend/src/components/aside/Navigation.tsx
+++ frontend/src/components/aside/Navigation.tsx
@@ -1,30 +1,48 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import Scrollspy from "react-scrollspy";
 import { HashLink } from "react-router-hash-link";
+import { NavLink } from "react-router-dom";
+
+import { removeBodyClassHandler } from "../../utils/domHandler";
 
 const Navigation: React.FC = () => {
+  const clickHandler = () => {
+    removeBodyClassHandler("offcanvas");
+  };
+
   return (
     <nav className="main-menu">
       <Scrollspy items={["home", "about", "services", "contact"]} currentClassName="active">
         <li>
-          <HashLink to="/#root">Home</HashLink>
+          <HashLink to="/#root" onClick={clickHandler}>
+            Home
+          </HashLink>
         </li>
         <li>
-          <HashLink to="/#about">That's me</HashLink>
+          <HashLink to="/#about" onClick={clickHandler}>
+            That's me
+          </HashLink>
         </li>
         <li>
-          <HashLink to="/#services">Angebot</HashLink>
+          <HashLink to="/#services" onClick={clickHandler}>
+            Angebot
+          </HashLink>
         </li>
         <li>
-          <HashLink to="/#contact">Kontakt</HashLink>
+          <HashLink to="/#contact" onClick={clickHandler}>
+            Kontakt
+          </HashLink>
         </li>
         <hr />
         <li>
-          <NavLink to="/impressum">Impressum</NavLink>
+          <NavLink to="/impressum" onClick={clickHandler}>
+            Impressum
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/datenschutz">Datenschutz</NavLink>
+          <NavLink to="/datenschutz" onClick={clickHandler}>
+            Datenschutz
+          </NavLink>
         </li>
       </Scrollspy>
     </nav>
