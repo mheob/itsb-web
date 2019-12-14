9c2107769c4724b4dab734e877e9e89d2bd8d17c
Alexander Böhm <tools@boehm.work>
2019-12-14 18:58:20 +0100

    Close #11: Generate and add the vCard

--- frontend/src/components/main/about/Actions.tsx
+++ frontend/src/components/main/about/Actions.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Actions: React.FC = () => {
   return (
     <section className="actions">
-      <a href="#!" className="btn btn-primary-outline">
+      <a href="/downloads/IT Service Böhm -- Alexander Böhm.vcf" className="btn btn-primary-outline">
         Download vCard
       </a>
       <a href="#contact" className="btn btn-primary">
