d578280e44d37c8cd742d6431b7f383ffd39b66e
Alexander Böhm <tools@boehm.work>
2019-12-26 16:30:37 +0100

    Remove copyright notice on the side footer

--- frontend/src/components/aside/Footer.tsx
+++ frontend/src/components/aside/Footer.tsx
@@ -6,7 +6,6 @@ import Social from "../shared/Social";
 const Header: React.FC = () => {
   return (
     <footer className="footer">
-      <p>Copyright &copy; {new Date().getFullYear()} All rights reserved</p>
       <Social links={socialData.links.map((link, _) => link)} />
     </footer>
   );
