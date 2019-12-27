5c0f0fc00a3fec124e6a33c8457209f3d1a8c952
Alexander Böhm <tools@boehm.work>
2019-12-27 19:01:55 +0100

    Open modal instead directly download the vcard

--- frontend/src/components/main/about/Actions.tsx
+++ frontend/src/components/main/about/Actions.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { HashLink } from "react-router-hash-link";
+
+import Modal from "../../shared/Modal";
 
 const Actions: React.FC = () => {
+  const [showModalState, setShowModalState] = useState(false);
+
+  const openModalHandler = () => {
+    setShowModalState(true);
+  };
+
+  const closeModalHandler = () => {
+    setShowModalState(false);
+  };
+
   return (
     <section className="actions">
-      <a href="/downloads/IT Service Böhm -- Alexander Böhm.vcf" className="btn btn-primary-outline">
+      <button className="btn btn-primary-outline" onClick={openModalHandler}>
         Download vCard
-      </a>
-      <a href="#contact" className="btn btn-primary">
+      </button>
+      <HashLink to="/#contact" className="btn btn-primary">
         Beauftrage mich
-      </a>
+      </HashLink>
+      <Modal show={showModalState} onCancel={closeModalHandler} header="Kontaktdaten herunterladen?">
+        <p>Möchtest Du dir die vCard von mir abspeichern?</p>
+        <p>
+          Du kannst sie Dir direkt in deinem Mail-Programm, wie z.B. Outlook oder Thunderbird abspeichern.
+          <br />
+          Dann hast Du meine Kontaktdaten immer griffbereit.
+        </p>
+        <p>
+          <a href="/downloads/IT Service Böhm -- Alexander Böhm.vcf" className="btn btn-primary">
+            vCard jetzt herunterladen
+          </a>
+        </p>
+      </Modal>
     </section>
   );
 };
