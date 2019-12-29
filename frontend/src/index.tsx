b3cffbf8dabdfbca9c6ddc5b86c4de6e516697f1
Alexander Böhm <tools@boehm.work>
2019-12-29 16:08:47 +0100

    Fix Matomo settings

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,15 +5,8 @@ import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
 import "./styles/css/index.css";
 import App from "./components/App";
 
-const matomoInstance = createInstance({
-  urlBase: "https://stats.its-boehm.de/",
-  siteId: 3,
-  trackerUrl: "https://stats.its-boehm.de/piwik.php",
-  srcUrl: "https://stats.its-boehm.de/piwik.js"
-});
-
 ReactDOM.render(
-  <MatomoProvider value={matomoInstance}>
+  <MatomoProvider value={createInstance({ urlBase: "https://stats.its-boehm.de/", siteId: 1 })}>
     <App />
   </MatomoProvider>,
   document.getElementById("root")
