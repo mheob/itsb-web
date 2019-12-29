58d33af0d60d219c5dbed90ca33417e0fe1f6d45
Alexander Böhm <tools@boehm.work>
2019-12-29 17:06:02 +0100

    Improve SEO

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
+import { HelmetProvider } from "react-helmet-async";
 
 import "./styles/css/index.css";
 import App from "./components/App";
+import Seo from "./components/shared/Seo";
 
 ReactDOM.render(
   <MatomoProvider value={createInstance({ urlBase: "https://stats.its-boehm.de/", siteId: 1 })}>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </MatomoProvider>,
   document.getElementById("root")
 );
