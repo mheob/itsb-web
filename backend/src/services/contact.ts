b27e5920618641b228cb13e1887dbaf936fdb994
Alexander Böhm <tools@boehm.work>
2020-01-14 17:03:18 +0100

    Add more details to the mails

--- backend/src/services/contact.ts
+++ backend/src/services/contact.ts
@@ -17,21 +17,25 @@ export const sendMail = async (data: Contact) => {
     }
   );
 
-  // TODO #40: Mail for the customer should be more detailed.
-
   const plainBody = `
+    Hallo ${data.name}!
+
     Vielen Dank für Deine Nachricht! Ich werde mich zeitnah bei Dir melden.
 
-    Kontaktdaten:
-    -------------
-    Name: ${data.name}
-    E-Mail: ${data.email}
-    Telefon: ${data.phone}
-    Datenschutz: ${data.privacy}
+    Die folgenden Daten hast Du mir übermittelt:
 
-    Nachricht:
-    ----------
-    ${data.message}
+    > Kontaktdaten:
+    > -------------
+    > Name: ${data.name}
+    > E-Mail: ${data.email}
+    > Telefon: ${data.phone}
+    > Datenschutz: ${data.privacy}
+    > 
+    > Nachricht:
+    > ----------
+    > ${data.message}
+
+    Ich werde mich zeitnah bei Dir melden.
 
     Freundliche Grüße aus Neuwied,
 
@@ -47,16 +51,21 @@ export const sendMail = async (data: Contact) => {
   `;
 
   const htmlBody = `
-    <p>Vielen Dank für Deine Nachricht! Ich werde mich zeitnah bei Dir melden.</p>
-    <h3>Kontaktdaten:</h3>
-    <ul>
-      <li>Name: ${data.name}</li>
-      <li>E-Mail: ${data.email}</li>
-      <li>Telefon: ${data.phone}</li>
-      <li>Datenschutz: ${data.privacy}</li>
-    </ul>
-    <h3>Nachricht:</h3>
-    <p>${data.message}</p>
+    <p>Hallo ${data.name}!</p>
+    <p>Vielen Dank für Deine Nachricht!</p>
+    <p>Die folgenden Daten hast Du mir übermittelt:</p>
+    <blockquote>
+      <h3>Kontaktdaten:</h3>
+      <ul>
+        <li>Name: ${data.name}</li>
+        <li>E-Mail: ${data.email}</li>
+        <li>Telefon: ${data.phone}</li>
+        <li>Datenschutz: ${data.privacy}</li>
+      </ul>
+      <h3>Nachricht:</h3>
+      <p>${data.message}</p>
+    </blockquote>
+    <p>Ich werde mich zeitnah bei Dir melden.</p>
     <p>Freundliche Grüße aus Neuwied,</p>
     <p><b><span style='font-size:12.0pt;font-family:"Lucida Handwriting"'>Alexander Böhm</span></b></p>
     <p><a href="mailto:ab@its-boehm.de">ab@its-boehm.de</a> • <a href="tel:+491608206654">+49 160 8206654</a></p>
