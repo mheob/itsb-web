1f403c6fb6f1ef205b759992984725f340f45ba5
Alex Böhm <tools@alex-boehm.dev>
2026-01-01 16:53:26 +0100

    feat: use `PUBLIC_VERCEL_PROJECT_PRODUCTION_URL`

--- astro.config.ts
+++ astro.config.ts
@@ -36,6 +36,6 @@ export default defineConfig({
 	],
 	output: 'static',
 	prefetch: { prefetchAll: true },
-	site: env.BASE_URL,
+	site: (nodeEnv === 'production' ? 'https://' : 'http://') + env.PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
 	trailingSlash: 'never',
 });
